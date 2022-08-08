4a0ae92ff3272d59704b69f8538eea0c7dfb44ee
Jennie Duffer <jenniej297@gmail.com>
2022-08-08 11:36:04 -0500

    initial commit

---
+++ MethodExercise/Methods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise
+{
+    internal class Methods
+    {
+        public static int Add(int x, int y)
+        { 
+            return x + y; 
+        }
+        public static int Multiply(int x, int y)
+        { 
+            return x * y;
+        
+        }
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+
+        }
+        
+        public static int Modulus(int x, int y)
+        {
+            return (x % y) * y;
+        }
+
+      
+    }
+  
+}
---
+++ MethodExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MethodExercise;
+
+//Console.WriteLine("What is your name?");
+//string name = Console.ReadLine();
+//Console.WriteLine(name);
+
+//Console.WriteLine("What is your favorite color?");
+//string color = Console.ReadLine();
+//Console.WriteLine(color);
+
+
+//Console.WriteLine("What is your favorite animal?");
+//string animal = Console.ReadLine();
+//Console.WriteLine(animal);
+
+//Console.WriteLine("What is your favorite band?");
+//string band = Console.ReadLine();
+//Console.WriteLine(band);
+
+//Console.WriteLine($"My name is {name}, my favorite color is {color}, my favorite animal is {animal}, and my favorite band is {band}.");
+
+Console.WriteLine(Methods.Add(2, 2));
+
+Console.WriteLine(Methods.Multiply(2, 2));
+
+Console.WriteLine(Methods.Subtract(2,2));
+
+Console.WriteLine(Methods.Divide(2, 2));
+
+Console.WriteLine(Methods.Modulus(2, 2));
+
+
+
